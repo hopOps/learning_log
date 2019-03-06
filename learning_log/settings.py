9f32e65c50bd95c46a9960a067d6c1002a2af0a0
unknown <f.collen@gmail.com>
2019-03-06 20:37:16 +0100

    add requirements

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'learning_log',
+        'USER': 'learning_log',
+        'PASSWORD': 'learning_log',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
